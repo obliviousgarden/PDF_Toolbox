395947f681ee44913a6f8f687e7f6f86c9086efb
ObliviousGarden <33819473+obliviousgarden@users.noreply.github.com>
2023-02-13 21:11:09 +0900

    2nd .gitignore and some default state of GUI

--- app/pdf_app.py
+++ app/pdf_app.py
@@ -1,8 +1,25 @@
 from PyQt5 import QtCore, QtWidgets
 import os, sys
-class PDF_Toolbox(Ui_MainWindow):
-    pass
 
+from pdf_toolbox_ui import Ui_MainWindow
+
+
+class PDF_Toolbox(Ui_MainWindow):
+    def __init__(self,parent=None):
+        super(PDF_Toolbox, self).__init__()
+        # init
+
+
+    def setupUi(self, MainWindow):
+        # Father's UI
+        Ui_MainWindow.setupUi(self, MainWindow)
+        # Menu
+        self.actionQuit.triggered.connect(on_Action_quit)
+        #
+
+def on_Action_quit():
+    sys.exit(0)
+    pass
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     mainWindow = QtWidgets.QMainWindow()
